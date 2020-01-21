bc71e929680ab98f4e52ec605a58e81fac45be6e
Alexey Shatalin <xiaouending@ya.ru>
2020-01-21 18:38:10 +0300

    Set utm params to registration link

--- src/components/Advantages.vue
+++ src/components/Advantages.vue
@@ -13,7 +13,7 @@
     </div>
 
     <div class="text-center my-16">
-      <a :href="`https://frogogo.ru/users/sign_up?promo_code=${promocode}`"
+      <a v-bind:href="`https://frogogo.ru/users/sign_up?promo_code=${promocode}${utmParams}`"
          class="bg-primary text-white hover:bg-secondary font-bold px-10 py-3 rounded uppercase">
          Зарегистрироваться
        </a>
@@ -50,7 +50,44 @@ export default {
           description: 'Сумма любой покупки на frogogo вернётся на ваш бонусный счёт. Участвуйте в акциях наших партнёров и сканируйте чеки, чтобы получить ещё больше бонусных рублей.'
         }
       ],
-      promocode: 'BONUS'
+      promocode: 'BONUS',
+      utmParams: ''
+    }
+  },
+  mounted() {
+    if (!localStorage.getItem('utmParams')) {
+      this.saveUtmParams()
+    }
+
+    this.setUtmParamsToRegistrationLink()
+  },
+  methods: {
+    saveUtmParams() {
+      const urlParams = new URLSearchParams(window.location.search)
+      const keyword = 'utm_'
+      let utmParams = {}
+
+      for (const [key, value] of urlParams.entries()) {
+        if (key.includes(keyword)) {
+          utmParams[key] = value
+        }
+      }
+
+      if (!this.isEmptyObject(utmParams)) {
+        localStorage.setItem('utmParams', JSON.stringify(utmParams))
+      }
+    },
+    setUtmParamsToRegistrationLink() {
+      const utmParams = JSON.parse(localStorage.getItem('utmParams'))
+
+      if (!utmParams) return
+
+      for (const [key, value] of Object.entries(utmParams)) {
+        this.utmParams +=`?${key}=${value}`
+      }
+    },
+    isEmptyObject(object) {
+      return Object.keys(object).length === 0
     }
   }
 }
